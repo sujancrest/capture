9726ce0877ca3978deb40c3a5efdc1a8aaecd905
Sujan Shrestha <shresthasuzzan0@gmail.com>
2020-12-24 15:07:49 +0545

    Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,54 @@
+import react from "react";
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+                        autem accusamus ex laboriosam porro, adipisci quam voluptatum
+                        magnam placeat corporis.
+            </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+                        autem accusamus ex laboriosam porro, adipisci quam voluptatum
+                        magnam placeat corporis.
+            </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+                        autem accusamus ex laboriosam porro, adipisci quam voluptatum
+                        magnam placeat corporis.
+            </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+                        autem accusamus ex laboriosam porro, adipisci quam voluptatum
+                        magnam placeat corporis.
+            </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FaqSection
